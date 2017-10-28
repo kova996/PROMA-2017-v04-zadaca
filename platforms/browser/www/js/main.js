c95ef8916c43428eb6747183afe817caeae2c411
kova996 <lukakovacusic@gmail.com>
2017-10-29 00:22:30 +0200

    First setup for main.js file

---
+++ platforms/browser/www/js/main.js
@@ -0,0 +1,18 @@
+var id = "ok3pcm0j2ko5";
+
+window.onload = function(){ //početak rada programa
+    gameStartSetter();
+    //kada su svi uredjaji spremni započni sa izvršavanjem glavnog programa
+    document.addEventListener("deviceReady",function(){
+
+
+
+
+
+    },false);
+
+    function gameStartSetter(){
+        var elementi = document.getElementById("game-container");
+        elementi.style.display="none";
+    }
+}
